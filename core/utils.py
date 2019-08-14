50feffe8c7b16b5b0be07cf825aa37173d20a8f8
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-08-14 17:45:44 +0200

    Update utils.py

--- core/utils.py
+++ core/utils.py
@@ -60,7 +60,7 @@ class utils:
     @staticmethod
     def banner():
         utils.Go("\033[H\033[J")
-        utils.Color['redBold']
+        utils.Go(utils.Color['redBold'])
         os.system("cd && cat GeoSpy/banner/banner.txt")
         print("")
         utils.Go("\t" + utils.Color['redBold'] + "Geolocation Spy" + utils.Color['white'] + " By " + utils.Color['whiteBold'] + "Ivan Nikolsky" + utils.Color['white'] + " (" + utils.Color['blue'] + "@entynetproject" + utils.Color['white'] + ")" + utils.Color['white'])
