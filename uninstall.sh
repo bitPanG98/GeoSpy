4d557ef78b15ff0685abd8570ac5556ca846c733
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-08-19 15:46:24 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -14,7 +14,7 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations under
 # the License.
-
+#
 # Disclaimer:
 # This tool has been published educational purposes. It is intended to teach 
 # people how bad guys could track them, monitor them or obtain information from 
