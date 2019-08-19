3d251983adef0c2fc217bc54c22a7e4051eb56a7
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-08-19 15:38:29 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,27 @@
+#! /bin/bash
+
+# Copyright (C) 2016 - 2018 Entynetproject, Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use the software except in compliance with the License.
+
+# You may obtain a copy of the License at:
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+# Disclaimer:
+# This tool has been published educational purposes. It is intended to teach 
+# people how bad guys could track them, monitor them or obtain information from 
+# their credentials, we are not responsible for the use or the scope that someone 
+# may have through this project. We are totally convinced that if we teach how 
+# vulnerable things really are, we can make the Internet a safer place.
+
 RSA="\033[31m"
 YSA="\033[1;93m"
 CEA="\033[0m"
