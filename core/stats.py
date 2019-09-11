db9025b789f3bf5fc309f58bccee2aa51382c2f0
Ivan Nikolsky <54115104+entydev@users.noreply.github.com>
2019-09-11 23:37:57 +0300

    Update stats.py

--- core/stats.py
+++ core/stats.py
@@ -7,9 +7,9 @@
 #########
 #
 # GeoSpy depends of this file
-# For full copyright information this visit: https://github.com/entynetproject/GeoSpy
+# For full copyright information this visit: https://github.com/entynetproject/geospy
 #
-# Copyright 2018 by Ivan Nikolsky (@entynetproject) / <entynetproject@gmail.com>
+# Copyright 2018 by Entynetproject / <entynetproject@gmail.com>
 #**
 from core.dependence import urllib2
 import sys
