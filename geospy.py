5236f78b88fcfeb84e3555e823ac1a9b9a6cb309
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-10-03 01:59:08 +0300

    Update geospy.py

--- geospy.py
+++ geospy.py
@@ -45,7 +45,7 @@
                                               #
 import os                                     #
                                               #
-printf '\033]2;GeoSpy Console\a'              #
+os.system("printf '\033]2;GeoSpy Console\a'") #
                                               #
 from core.utils import utils                  #
 from core.geospy import GeoSpy                #
