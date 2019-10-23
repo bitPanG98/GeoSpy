1a23e1d9fa33fc384114bbe37b124a1bd474bc66
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-10-23 17:36:03 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -104,14 +104,14 @@ fi
 fi
 
 else
-read -e -p $'\033[34m- Select your architecture (amd/intel/arm) \033[33m~> ' CONF
+read -e -p $'\033[34m- \033[0mSelect your architecture (amd/intel/arm) \033[33m~> ' CONF
 if [[ "$CONF" = "" ]]
 then
 exit
 else
 if [[ "$CONF" = "arm" ]]
 then
-read -e -p $'\033[34mIs this a single board computer (yes/no)? \033[33m~> ' PI
+read -e -p $'\033[34m- \033[0mIs this a single board computer (yes/no)? \033[33m~> ' PI
 if [[ "$PI" = "yes" ]]
 then
 echo "amd" >> /etc/geospy.conf
