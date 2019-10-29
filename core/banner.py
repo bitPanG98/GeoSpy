bf313157a73b33cde287652820c94c2a10075aca
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-10-29 19:39:56 +0300

    Update banner.py

--- core/banner.py
+++ core/banner.py
@@ -1,10 +1,25 @@
 import os
 
+Color = {
+      "cyan": Style.NORMAL+Fore.CYAN,
+      "cyanBold": Style.BRIGHT+Fore.CYAN,
+      "blue": Fore.BLUE,
+      "blueBold": Style.BRIGHT+Fore.BLUE,
+      "red": Style.NORMAL+Fore.RED,
+      "redBold": Style.BRIGHT+Fore.RED,
+      "green": Style.NORMAL+Fore.GREEN,
+      "greenBold": Style.BRIGHT+Fore.GREEN,
+      "white": Style.NORMAL+Fore.WHITE,
+      "whiteBold": Style.BRIGHT+Fore.WHITE,
+      "yellow": Style.NORMAL+Fore.YELLOW,
+      "yellowBold": Style.BRIGHT+Fore.YELLOW
+}
+
 print("\033[H\033[J")
 print(redBold)
 os.system("cat banner/banner.txt")
 print("")
-print("\t" + redBold + "Geolocation Spy" + white + " by " + whiteBold + "Entynetproject" + white + "" + blue + "" + white + "" + white)
+print("\t" + Color.['redBold'] + "Geolocation Spy" + white + " by " + whiteBold + "Entynetproject" + white + "" + blue + "" + white + "" + white)
 print("\t" + "-------------------------------------------------")
 print("\t" + "|" + green + " People tracker on internet for OSINT research " + white + "|" + white)
 print("\t" + "-------------------------------------------------")
