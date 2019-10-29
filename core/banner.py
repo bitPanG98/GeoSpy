1542255a13888a436b08c5de77bcbd2bff9cd43d
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-10-29 19:37:59 +0300

    Create banner.py

---
+++ core/banner.py
@@ -0,0 +1,12 @@
+import os
+
+print("\033[H\033[J")
+print(redBold)
+os.system("cat banner/banner.txt")
+print("")
+print("\t" + redBold + "Geolocation Spy" + white + " by " + whiteBold + "Entynetproject" + white + "" + blue + "" + white + "" + white)
+print("\t" + "-------------------------------------------------")
+print("\t" + "|" + green + " People tracker on internet for OSINT research " + white + "|" + white)
+print("\t" + "-------------------------------------------------")
+print("\t" + "| " + white + "v" + redBold + "2.0" + white + " |")    
+print("\t" + "--------" + "\n")
