fe7ae0d09f1a847784d2cf86109d29184ad19886
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-12-09 02:23:20 +0300

    Update utils.py

--- core/utils.py
+++ core/utils.py
@@ -1,16 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#**
-#
-#########
-# GeoSpy #
-#########
-#
-# GeoSpy depends of this file
-# For full copyright information this visit: https://github.com/entynetproject/geospy
-#
-# Copyright 2018 by Entynetproject / <entynetproject@gmail.com>
-#**
+
 import random
 import hashlib
 import threading
