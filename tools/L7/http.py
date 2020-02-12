10111aae69fbb84e2d794ccc3414fdae865f015d
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-02-12 13:58:33 +0300

    Update http.py

--- tools/L7/http.py
+++ tools/L7/http.py
@@ -1,3 +1,27 @@
+#!/usr/bin/env python3
+
+# MIT License
+#
+# Copyright (C) 2019-2020, Entynetproject. All Rights Reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import requests
 import random
 import time
@@ -12,10 +36,10 @@ def HTTP_ATTACK(threads, attack_time, target):
 	FINISH = False
 
 	if ipTools.isCloudFlare(target):
-		if not input("[?] Current site is under CloudFlare protection. Do you want to continue? (y/n)\n >>> ") in ("y", "Y", "1"):
+		if not input("[WARNING] Current site is under CloudFlare protection. Do you want to continue? (y/n)\n >>> ") in ("y", "Y", "1"):
 			exit()
 
-	print("[#] Attack started for " + str(attack_time) + " secounds..")
+	print("[BEGIN] Attack started for " + str(attack_time) + " seconds...")
 	
 	threads_list = []
 	# Load 25 random user agents
@@ -50,7 +74,7 @@ def HTTP_ATTACK(threads, attack_time, target):
 
 	# Start threads
 	for thread in range(0, threads):
-		print("[#] Staring thread " + str(thread))
+		print("[BEGIN] Staring thread " + str(thread) + "...")
 		t = Thread(target = http_flood)
 		t.start()
 		threads_list.append(t)
@@ -61,4 +85,4 @@ def HTTP_ATTACK(threads, attack_time, target):
 		FINISH = True
 		thread.join()
 	
-	print("[!] HTTP attack stopped!")
+	print("[WARNING] HTTP attack stopped!")
