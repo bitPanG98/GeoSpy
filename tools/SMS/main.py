98cda0347881d8e406a1aba7b4e3abb906b55591
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-02-12 13:33:05 +0300

    Update main.py

--- tools/SMS/main.py
+++ tools/SMS/main.py
@@ -17,7 +17,7 @@ def SMS_ATTACK(threads, attack_time, phone):
 	phone = number.normalize(phone)
 	# Get country name by phone
 	country = number.getCountry(phone)
-	print("[ BEGIN ] Starting SMS flood to number: " + phone + ", country: " + country + ", time: " + str(attack_time) + " seconds...")
+	print("[BEGIN] Starting SMS flood to number: " + phone + ", country: " + country + ", time: " + str(attack_time) + " seconds...")
 
 	# Send SMS
 	def sms_flood():
@@ -29,7 +29,7 @@ def SMS_ATTACK(threads, attack_time, phone):
 
 	# Start threads
 	for thread in range(threads):
-		print("[ BEGIN ] Starting thread " + str(thread) + "...")
+		print("[BEGIN] Starting thread " + str(thread) + "...")
 		t = Thread(target = sms_flood)
 		t.start()
 		threads_list.append(t)
