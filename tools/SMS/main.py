61aba76707a818f15f0a2a66f71578979723cbc4
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-02-12 17:45:01 +0300

    Update main.py

--- tools/SMS/main.py
+++ tools/SMS/main.py
@@ -41,7 +41,7 @@ def SMS_ATTACK(threads, attack_time, phone):
 	phone = number.normalize(phone)
 	# Get country name by phone
 	country = number.getCountry(phone)
-	print("\033[1;34m"+"[*]"+"\033[0m"+" Starting SMS flood to number: " + phone + ", country: " + country + ", time: " + str(attack_time) + " seconds...")
+	print("\033[1;34m"+"[*]"+"\033[0m"+" Starting SMS flood to number: " + phone + "...")
 
 	# Send SMS
 	def sms_flood():
