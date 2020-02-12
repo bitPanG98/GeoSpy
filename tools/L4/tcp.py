42ab4266c9e5e8743ad008503be6099f466ced2e
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-02-12 23:12:30 +0300

    Update tcp.py

--- tools/L4/tcp.py
+++ tools/L4/tcp.py
@@ -34,7 +34,7 @@ def TCP_ATTACK(threads, attack_time, target):
 	target_ip = target.split(":")[0]
 	target_port = int(target.split(":")[1])
 
-	print("\033[1;34m"+"[*]"+"\033[0m"+" Starting attack...")
+	print("\033[1;34m"+"[*]"+"\033[0m"+" Starting TCP attack...")
 	
 
 	threads_list = []
